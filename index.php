2824e4fe8c5f6ae005feefe981ac6ebd04160e05
Varenik575 <invisibleredhead5@gmail.com>
2023-12-29 01:46:50 +0200

    second commit, added styles, added html

--- index.php
+++ index.php
@@ -4,20 +4,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css" />
     <title>Document</title>
 </head>
 
 <body>
 
+    <header class="lab-header">
+        <h1 class="lab-head">Лабораторна робота №4, варіант №4</h1>
+        <p class="lab-subhead">на тему "Основи мови PHP"</p>
+        <p class="lab-subhead">Виконав ст. гр. КІУКІ-21-3 Букреева О.С.</p>
+    </header>
+
     <?php
     $csvFilePath = 'colors.csv';
     $rows = array_map('str_getcsv', file($csvFilePath));
-    echo '<table border="1">';
+    echo '<table>';
 
     echo '<tr><th>Color</th><th>Hex Code</th></tr>';
 
     foreach ($rows as $row) {
         echo '<tr>';
+
         $backgroundColor = $row[0];
 
         // не придумала, як залишити верхні назви стовпців 
